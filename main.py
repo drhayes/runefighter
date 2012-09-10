210ca896c24c305a53804488b547e30847064c40
David Hayes <drhayes@gmail.com>
2012-09-09 23:49:31 -0700

    Ship now moves side-to-side.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pyglet
 from pyglet.gl import *
+from pyglet.window import key
 
 
 import constants
@@ -17,6 +18,36 @@ class Player(object):
   def __init__(self, lives=3, score=0):
     self.lives = lives
     self.score = score
+
+
+class PlayerShip(object):
+  """The actual moving, self-drawing thing."""
+  def __init__(self, ship_sprite):
+    self.ship_sprite = ship_sprite
+    self.moving_right = False
+    self.moving_left = False
+
+  def draw(self):
+    self.ship_sprite.draw()
+
+  def on_key_press(self, symbol, modifiers):
+    if symbol == key.RIGHT:
+      self.moving_right = True
+    if symbol == key.LEFT:
+      self.moving_left = True
+
+  def on_key_release(self, symbol, modifiers):
+    if symbol == key.RIGHT:
+      self.moving_right = False
+    if symbol == key.LEFT:
+      self.moving_left = False
+
+  def update(self, dt):
+    if self.moving_left and self.ship_sprite.x > 50:
+      self.ship_sprite.x -= int(constants.SHIP_SPEED * dt)
+    if self.moving_right and self.ship_sprite.x < constants.SCREEN_WIDTH - 50:
+      self.ship_sprite.x += int(constants.SHIP_SPEED * dt)
+
 
 
 # Kicks everything off.
@@ -43,6 +74,12 @@ def main():
   # Load sprites.
   ship = pyglet.sprite.Sprite(res_man.ship, x=50, y=70)
 
+  # The player-controlled ship.
+  player_ship = PlayerShip(ship)
+  window.push_handlers(player_ship)
+  # Schedule updates so the thing actually moves.
+  pyglet.clock.schedule(player_ship.update)
+
   # Get some text.
   res_man.create_label('press_space', 'Press Space To Continue',
     x=constants.SCREEN_WIDTH / 2, y=120)
@@ -62,7 +99,7 @@ def main():
   # State manager and states.
   state_manager = states.StateManager(window, starfield)
 
-  shooting_at_things = states.ShootingAtThings(player, res_man, ship)
+  shooting_at_things = states.ShootingAtThings(player, res_man, player_ship)
   transition_to_shooting = state_manager.create_transition(shooting_at_things)
   get_ready = states.GetReady(player, res_man, transition_to_shooting)
   transition_to_ready = state_manager.create_transition(get_ready)
